3d038479ef1c58727bcc66769d428a620d307336
Maria Cuan <esthercuan@gmail.com>
2016-11-19 20:47:28 +0100

    created media queries and styled the landing page

---
+++ src/utils/mediaqueries.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { css } from 'styled-components';
+
+const sizes = {
+  giant: 1170,
+  desktop: 992,
+  tablet: 900,
+  phone: 500,
+}
+
+// iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((accumulator, label) => {
+  // use em in breakpoints to work properly cross-browser and support users
+  const emSize = sizes[label] / 16
+  accumulator[label] = (...args) => css`
+    @media (max-width: ${emSize}em) {
+      ${css(...args)}
+    }
+  `
+  return accumulator
+}, {})
