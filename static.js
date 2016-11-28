cb46dbc87c1dd6aa84f87fe3d2cf311c63278533
Kamal Mango <kamal.mango7@gmail.com>
2016-11-28 02:02:56 +0100

    add static file for heroku routing

---
+++ static.js
@@ -0,0 +1,9 @@
+{
+  "root": "build/",
+  "clean_urls": false,
+  "routes": {
+    "/": "src/containers/Landing.jsx",
+    "/courses": "src/containers/Courses.jsx",
+    "/coursePage": "src/containers/CoursePage.jsx"
+  }
+}
