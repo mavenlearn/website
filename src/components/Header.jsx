fb4c8ef507b125d0c76b8a4726a1d4b97558797b
Kamal Mango <kamal.mango7@gmail.com>
2016-12-08 05:03:50 +0100

    add contact modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import LogoImage from '../containers/img/logoMaven4.png';
 import LogoImage2 from '../containers/img/gridLogoBlack.png';
 import LogoImage3 from '../containers/img/logoMaven2.png';
 import { Link } from 'react-router';
+import { Modal } from 'react-bootstrap';
 
 const Container = styled.nav`
   z-index: 10;
@@ -78,18 +79,139 @@ const List = styled.ul`
   margin-left: auto;
 `;
 
-const Header = () => (
-  <Container>
-    <Link to='/'><Logo src={LogoImage3}></Logo></Link>
-    <List>
-      <ListItem><Link className='headLink' to='/courses'>Courses</Link></ListItem>
-      <ListItem><Link className='headLink' to='/apprentice'>Apprentice</Link></ListItem>
-      <ListItem><Link className='headLink' to='/immersive'>Immersive</Link></ListItem>
-      <ListItem><Link className='headLink' to='/workshop'>Workshop</Link></ListItem>
-      <ListItem><Button href='https://mavevlearn.typeform.com/to/WdIlPD' target='_blank'>Apply Now</Button></ListItem>
-    </List>
-  </Container>
-);
+const SocialList = styled.ul`
+  padding: 0;
+`;
+
+const Facebook = styled.i `
+  padding:10px 14px;
+  padding-left: 0;
+  transition: .5s;
+  background-color: white;
+
+  &:hover {
+    background-color: #3d5b99;
+    color: white;
+  }
+`;
+
+const Twitter = styled.i `
+  padding:10px 14px;
+  transition: .5s;
+  background-color: white;
+
+  &:hover {
+    background-color: #00aced;
+    color: white;
+  }
+`;
+
+const Linkedin = styled.i `
+  padding:10px 14px;
+  transition: .5s;
+  background-color: white;
+
+  &:hover {
+    background-color: #0073a4;
+    color: white;
+  }
+`;
+
+const SocialIcon = styled.li `
+  vertical-align: top;
+  display: inline;
+  height: 100px;
+`;
+
+const IconLink = styled.a `
+  color: lightgrey;
+  text-decoration: none;
+  color: black;
+`;
+
+const ModalTitle = styled.h2 `
+  text-align: center;
+  font-size: 4em;
+`;
+
+const Section = styled.div `
+  margin-bottom: 5vh;
+  margin-left: 3vw;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const SectionTitle = styled.h2 `
+  font-size: 2.5em;
+`;
+
+const SectionInfo = styled.p `
+  color: black;
+`;
+
+const ModalLogo = styled.img`
+  height: 5vh;
+`;
+
+const Header = React.createClass({
+  getInitialState() {
+    return { showModal: false };
+  },
+
+  close() {
+    this.setState({ showModal: false });
+  },
+
+  open() {
+    this.setState({ showModal: true });
+  },
+
+  render() {
+    return (
+      <Container>
+        <Link to='/'><Logo src={LogoImage3}></Logo></Link>
+        <List>
+          <ListItem><Link className='headLink' to='/courses'>Courses</Link></ListItem>
+          <ListItem><Link className='headLink' onClick={this.open}>Contact Us</Link></ListItem>
+          <ListItem><Button href='https://mavevlearn.typeform.com/to/WdIlPD' target='_blank'>Apply Now</Button></ListItem>
+        </List>
+
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <ModalTitle>Contact Us</ModalTitle>
+          </Modal.Header>
+          <Modal.Body>
+            <Section>
+              <SectionTitle>email</SectionTitle>
+              <SectionInfo>hello@mavenlearn.com</SectionInfo>
+            </Section>
+            <Section>
+              <SectionTitle>phone</SectionTitle>
+              <SectionInfo>+34 635 176 956</SectionInfo>
+            </Section>
+            <Section>
+              <SectionTitle>location</SectionTitle>
+              <SectionInfo>Ramblas 40-42&nbsp;08002,&nbsp;Barcelona,&nbsp;Reial Coworking</SectionInfo>
+            </Section>
+            <Section>
+              <SectionTitle className='social'>social</SectionTitle>
+              <SocialList>
+                <SocialIcon><IconLink href="https://www.facebook.com/MavenLearn/" target="_blank"> <Facebook className="fa fa-facebook"></Facebook></IconLink></SocialIcon>
+                <SocialIcon><IconLink href="https://twitter.com/maven_learn" target="_blank"> <Twitter className="fa fa-twitter"></Twitter></IconLink></SocialIcon>
+                <SocialIcon><IconLink href="https://www.linkedin.com/company/maven-learn?report%2Esuccess=KJ_KkFGTDCfMt-A7wV3Fn9Yvgwr02Kd6AZHGx4bQCDiP6-2rfP2oxyVoEQiPrcAQ7Bf" target="_blank"> <Linkedin className="fa fa-linkedin"></Linkedin></IconLink></SocialIcon>
+              </SocialList>
+            </Section>
+          </Modal.Body>
+          <Modal.Footer>
+            <ModalLogo src={LogoImage3}></ModalLogo>
+          </Modal.Footer>
+        </Modal>
+      </Container>
+    )  
+  }
+});
 
 export default Header;
 
