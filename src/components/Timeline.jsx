bcf5026324e73aac91ddfd3f6082ea0ae08e22b0
Maria Cuan <esthercuan@gmail.com>
2016-12-13 13:32:32 -0600

    roadmap built

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -13,15 +13,41 @@ const Container = styled.div`
 
 const Title = styled.h2`
   padding-top: 6vh;
+  color: white;
 `;
 
 const TimeLine = styled.div`
   width:100%;
-  text-align:center;
-  width: 72%;
-  padding: 150px;
-  margin: auto;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content:center;
+`;
+
+const Box = styled.div`
+  width: 20%;
+  height: 20vw;
+  margin-top: 7vh;
+  margin-bottom: 10vh;
+  background-color:${props => props.one ? '#3A3E4A' : props.two ? '#444A5A' : props.three ? '#3A3E4A' : 'rgb(43, 45, 51)'} ;
+
+`;
+
+const BoxTitle = styled.p`
+  text-transform: uppercase;
+  color: rgba(225,225,225,0.5);
+  font-size: 1.3rem;
+  letter-spacing: 0.1rem;
+  font-weight: 100;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+  background-color: rgba(0,0,0,0.1);
+`;
+const BoxList = styled.p`
+  color: rgba(225,225,225,0.5);
+  padding-left: 1.5rem;
+  font-weight: 100;
+  font-size: 1.3rem;
+  text-align: left;
 `;
 const Img  = styled.img`
   stroke-dasharray: 1000;
@@ -34,7 +60,25 @@ const Timeline = () => (
   <Container>
     <Title>Engineer Roadmap</Title>
     <TimeLine>
-      <Img src={timeline} />
+      <Box one>
+        <BoxTitle>Apprentice 0-20</BoxTitle>
+        <BoxList>Basic Javascript</BoxList>
+        <BoxList>Debugging</BoxList>
+        <BoxList>Problem Solving Strategies</BoxList>
+      </Box>
+      <Box two>
+        <BoxTitle>Immersive 20-100</BoxTitle>
+        <BoxList>Full-Stack Javascript</BoxList>
+        <BoxList>Servers + Databases</BoxList>
+        <BoxList>Frameworks</BoxList>
+        <BoxList>Best Practices</BoxList>
+      </Box>
+      <Box three>
+        <BoxTitle>Apprentice 0-20</BoxTitle>
+        <BoxList>Basic Javascript</BoxList>
+        <BoxList>Debugging</BoxList>
+        <BoxList>Problem Solving Strategies</BoxList>
+      </Box>
     </TimeLine>
   </Container>
 );
