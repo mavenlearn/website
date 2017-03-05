c6bdc29d1807c8a5ed50a173ecc61ec36be98588
Kamal Mango <kamal.mango7@gmail.com>
2017-03-05 18:13:13 +0100

    add and hide link for sitemap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Router, Route, browserHistory } from 'react-router';
 import Landing from './containers/Landing';
 import Courses from './containers/Courses';
 import CoursePage from './containers/CoursePage';
-import fsgTxt from './fsg8d633.txt'
 
 const App = () => (
   <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
@@ -13,7 +12,6 @@ const App = () => (
       <Route path='/apprentice' component={CoursePage} />
       <Route path='/immersive' component={CoursePage} />
       <Route path='/workshop' component={CoursePage} />
-      <Route path='/fsg8d633.txt' component={fsgTxt} />
     </div>
   </Router>
 );
